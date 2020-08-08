798dd969fb1e20d9581f2c72a59a00e0f5fa4b85
TruongDX8 <truonghusk17@gmail.com>
2020-08-08 16:20:26 +0700

    first commit

---
+++ src/main/java/example/HelloWorld.java
@@ -0,0 +1,61 @@
+package example;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.xml.ws.Endpoint;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebService()
+public class HelloWorld {
+
+//  Connection connection = ConnectionUtil.getConnection();
+
+  @WebMethod
+  public String sayHelloWorldFrom(String from) {
+    String result = "Hello, world, from " + from;
+    System.out.println(result);
+    return result;
+  }
+
+  @WebMethod
+  public int totalValue(int first, int second) {
+    int result = first+second;
+    System.out.println(result);
+    return result;
+  }
+
+//  @WebMethod
+//  public List<Room> getAllRooms(){
+//    List<Room> list = new ArrayList<>();
+//    String sql = "SELECT * FROM product";
+//    try {
+//      PreparedStatement ps = connection.prepareStatement(sql);
+//      ResultSet rs = ps.executeQuery();
+//      while (rs.next())
+//      {
+//        int id  = rs.getInt("id");
+//        String name = rs.getString("name");
+//        int priceperday = rs.getInt("priceperday");
+//        int pricepermonth = rs.getInt("pricepermonth");
+//        Room room = new Room(id,name,priceperday,pricepermonth);
+//        list.add(room);
+//      }
+//
+//    } catch (SQLException throwables) {
+//      throwables.printStackTrace();
+//    }
+//
+//    return list;
+//  }
+
+  public static void main(String[] argv) {
+    Object implementor = new HelloWorld ();
+    String address = "http://localhost:9000/HelloWorld";
+    Endpoint.publish(address, implementor);
+  }
+}
